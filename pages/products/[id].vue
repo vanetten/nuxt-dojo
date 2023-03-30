d2d58150b8bcd81ac541042716055496dfbbc7da
vanetten <bill@bioteam.net>
2023-03-30 16:55:19 -0400

    route paramters

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque mollitia recusandae deleniti itaque! Quibusdam, autem eligendi voluptas atque quo nam?</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
+
+<style lang="scss" scoped>
+
+</style>
