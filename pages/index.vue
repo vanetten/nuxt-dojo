cfc083049b4a289f9e91fe743277130fbb20350a
vanetten <bill@bioteam.net>
2023-03-30 13:28:41 -0400

    add pages

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod corrupti eius dignissimos porro quaerat mollitia tenetur enim vel laudantium maiores.
+    </p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod corrupti eius dignissimos porro quaerat mollitia tenetur enim vel laudantium maiores.
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
