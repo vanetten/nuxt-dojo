2192bbf9c835ab3ffd01322e40e714f232a3e336
vanetten <bill@bioteam.net>
2023-03-31 12:25:00 -0400

    add tailwind

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
-        <ul>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
+        <ul class="flex gap-4">
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
-          <li><NuxtLink to="/products">Products</NuxtLink></li>          
+          <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>          
         </ul>
       </nav>
     </header>
     <!--- output page content -->
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
   </div>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/products">Nuxt Dojo Merch</NuxtLink>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4">
+        <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merch</NuxtLink>
       </nav>
     </header>
     <!--- output page content -->
-    <div>
+    <div class="container mx-auto px-4">
       <slot />
     </div>
-    <footer>
-      <ul>
+    <footer class="container mx-auto p-4 flex justify-between border-t-2">
+      <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
         <li><NuxtLink to="/about">About</NuxtLink></li>
         <li><NuxtLink to="/products">Products</NuxtLink></li>          
