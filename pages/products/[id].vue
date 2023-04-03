34250179c31ccf91e61a6f1d15ad4330c96096da
vanetten <bill@bioteam.net>
2023-04-03 13:56:10 -0400

    add errors

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -10,6 +10,9 @@
   const uri = 'https://fakestoreapi.com/products/' + id
   // fetch the product, with key to force new fetch
   const { data: product } = await useFetch(uri, {key: id})
+  if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true})
+  }
   definePageMeta({
     layout: 'products'
   })
